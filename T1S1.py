7fc84c19589271b3e3f4ed5a5413c23aabbc5ca5
zahra-mohammadi79 <163278109+zahra-mohammadi79@users.noreply.github.com>
2024-04-23 23:31:08 +0330

    Add files via upload
    تمرین یک (سوال یک)

---
+++ T1S1.py
@@ -0,0 +1,8 @@
+# تمرین یک سوال یک
+tolerance = 1e-10
+
+
+if abs(0.1 + 0.2 - 0.3) < tolerance:
+    print("0.1 + 0.2 is approximately equal to 0.3")
+else:
+    print("0.1 + 0.2 is not equal to 0.3")
